8d4aeae7b7185e2b0fb56a4acdf8b60fcb4add60
Gerardo Salle Jr <126658034+ZA005@users.noreply.github.com>
2026-02-15 21:28:11 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,7 @@
+--// =========================
+--// SERVICES
+--// =========================
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
@@ -6,16 +10,34 @@ local player = Players.LocalPlayer
 local backpack = player:WaitForChild("Backpack") 
 local potionLimit = 1
 
+--// =========================
+--// KNIT
+--// =========================
+
 local Knit = ReplicatedStorage.Packages._Index["sleitnick_knit@1.7.0"].knit
 local ShopService = Knit.Services.ShopService
 
+local TeleportToShop = ShopService.RF.TeleportToShop
 local TeleportToPlot = ReplicatedStorage.Packages._Index["sleitnick_knit@1.7.0"].knit.Services.PlotService.RF.TeleportToPlot
 local GetAllCategoryStockRF = ShopService.RF.GetAllCategoryStock
 local BuyItemRF = ShopService.RF.BuyItemFromCurrency
 
+--// =========================
+--// EVENT
+--// =========================
 local EventService = Knit.Services.EventService
 local ContributeRF = EventService.RF.ContributeToEvent
 local GetTimeRemainingRF = EventService.RF.GetTimeRemaining
+
+
+--// =========================
+--// Coordinates
+--// =========================
+
+local positions = {
+    Vector3.new(-395.85, 14.22, 177.90),
+    Vector3.new(-389.90, 14.22, 25.10),
+}
 
 local function teleportTo(pos)
     local character = player.Character or player.CharacterAdded:Wait()
@@ -23,7 +45,10 @@ local function teleportTo(pos)
     humanoidRootPart.CFrame = CFrame.new(pos)
 end
 
-print("LOADED SUCCESSFULLY")
+--// =========================
+--// Main Loop
+--// =========================
+
 task.spawn(function()
     while true do
         local success, stock = pcall(function()
@@ -34,19 +59,75 @@ task.spawn(function()
             -- Loop 1: TimeBoost_Rare
             for itemName, amount in pairs(stock[3]) do
                 if itemName == "TimeBoost_Rare" and amount > 0 then
+                    teleportTo(positions[1])
 
                     for i = 1, 4 do
                         BuyItemRF:InvokeServer(3, "TimeBoost_Rare")
                         print("[PURCHASED] TimeBoost_Rare")
                         task.wait(1)
                     end
+
+                    -- TeleportToPlot:InvokeServer()
+                end
+
+                -- if itemName == "CloudBoost_Legendary" and amount > 0 then
+                --     teleportTo(positions[1])
+
+                --     for i = 1, 4 do
+                --         BuyItemRF:InvokeServer(3, "CloudBoost_Legendary")
+                --         print("[PURCHASED] CloudBoost_Legendary")
+                --         task.wait(1)
+                --     end
+
+                --     -- TeleportToPlot:InvokeServer()
+                -- end
+            end
+
+            for itemName, amount in pairs(stock[2]) do
+                if itemName == "Toadstool_Tree" and amount > 0 then
+                    teleportTo(positions[2])
+
+                    for i = 1, 4 do
+                        BuyItemRF:InvokeServer(2, "Toadstool_Tree")
+                        print("[PURCHASED] Toadstool_Tree")
+                        task.wait(1)
+                    end
+
+                    -- TeleportToPlot:InvokeServer()
+                end
+            end
+
+            -- Loop 2: Tiles
+            for itemName, amount in pairs(stock[1]) do
+                if itemName == "Valentine_Tile" and amount > 0 then
+                    teleportTo(positions[2])
+                    for i = 1, 3 do
+                        BuyItemRF:InvokeServer(1, "Valentine_Tile")
+                        print("[PURCHASED] Valentine_Tile")
+                        task.wait(1)
+                    end
+                    -- TeleportToPlot:InvokeServer()
+				elseif itemName == "Bush_Tile" and amount > 0 then
+                    teleportTo(positions[2])
+                    for i = 1, 3 do
+                        BuyItemRF:InvokeServer(1, "Bush_Tile")
+                        print("[PURCHASED] Bush_Tile")
+                        task.wait(1)
+                    end
                 end
             end
         end
+        -- print("[LOOP REFRESH]")
         task.wait(10) -- Wait 10 seconds before repeating
     end
 end)
 
+
+--// =========================
+--// AUTO-CONTRIBUTE SURGE
+--// =========================
+
+-- Gather all Time Potions in backpack
 while true do  
 
 	-- Get remaining event time
